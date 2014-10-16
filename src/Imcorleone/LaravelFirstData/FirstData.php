dacef9a1c35186d8bb225dac98cadb04693b3a5e
Gian Santillan <giansantillan18@gmail.com>
2014-10-17 02:05:07 +0800

    added composer fix

--- src/Imcorleone/LaravelFirstData/FirstData.php
+++ src/Imcorleone/LaravelFirstData/FirstData.php
@@ -61,6 +61,26 @@ class FirstData
 
 	}
 
+	public static function getCardType($creditCard) 
+	{
+		$cards = array(
+        "visa" => "(4\d{12}(?:\d{3})?)",
+        "amex" => "(3[47]\d{13})",
+        "jcb" => "(35[2-8][89]\d\d\d{10})",
+        "maestro" => "((?:5020|5038|6304|6579|6761)\d{12}(?:\d\d)?)",
+        "solo" => "((?:6334|6767)\d{12}(?:\d\d)?\d?)",
+        "mastercard" => "(5[1-5]\d{14})",
+        "switch" => "(?:(?:(?:4903|4905|4911|4936|6333|6759)\d{12})|(?:(?:564182|633110)\d{10})(\d\d)?\d?)",
+	    );
+
+	    $names = array("Visa", "American Express", "JCB", "Maestro", "Solo", "Mastercard", "Switch");
+	    $matches = array();
+	    $pattern = "#^(?:".implode("|", $cards).")$#";
+	    $result = preg_match($pattern, str_replace(" ", "", $creditCard), $matches);
+	 
+	    return ($result>0)?$names[sizeof($matches)-2]:false;
+	}
+
 	public function getErrorMessage()
 	{
 		return $this->firstDataApi->getErrorMessage();
